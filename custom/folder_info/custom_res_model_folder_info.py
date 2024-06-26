6b59f3d6c27c3a1a689d78fd5513c1f1e6b99dfe
xiaozw <756124359@qq.com>
2024-06-26 10:23:33 +0800

    first commit

---
+++ custom/folder_info/custom_res_model_folder_info.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'NTUXUWf+Ee6gZDDQQulAVT', '\xe7\xac\xac\xe4\xb8\x80\xe6\x89\xb9')], 'd': {}}
---
+++ custom/folder_info/custom_sequence_folder_info.py
@@ -0,0 +1,5 @@
+# -*- encoding:utf-8 -*-
+_reload_all = True
+
+
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'oIpUXkZnEe6sdjDQQuNW61', '\xe5\xba\x8f\xe5\x88\x97\xe5\xb8\xa7')], 'd': {}}
